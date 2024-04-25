4756a9119d89665a2f0dd4e47ad7d1f7f102e7de
Vladi4ekN <167472798+Vladi4ekN@users.noreply.github.com>
2024-04-25 22:00:06 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ["melon", "banana", "grape", "apple", "pineapple"]
+print(my_list)
+print(my_list[0])
+print(my_list[4])
+print(my_list[2:5])
+my_list[2] = "watermelon"
+print(my_list)
+
+my_dict ={'Дима': 'Белгород', 'Руслан': 'Москва', 'Влад': 'Белоярский'}
+print(my_dict)
+print(my_dict.get('Дима'))
+my_dict.update({'Витя': 'Брянск'})
+print(my_dict)
